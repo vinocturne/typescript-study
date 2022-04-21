0decf5589e9e091855c0b8a2563a9ce0d2b2e8f0
Ghunhee Shin <hago@rocketstaff.com>
2022-04-21 17:02:36 +0900

    Feat(Theme) : "추가 typescript로 theme 설정"
    d.ts는 디클리어파일로, ts에서 사용되는 타입을 정리해둔 파일이다.
    styled.d.ts에서 styled-components의 defaultTheme를 불러와 필요한 속성들을 넣어 확장시켜주고,
    theme.ts에서 확장된 속성들을 바탕으로 원하는 값들을 object 형태로 세팅해준다.
    세팅이 된 후 index.tsx에서 App을 감싸도록 ThemeProvider를 넣어주고 테마를 설정해준다.
    각 컴포넌트에서는 props.theme에 접근하여 원하는 테마 속성들을 받아올 수 있다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
+import styled from 'styled-components';
+
+
+//지정된 ThemeProvider에서 값을 가져와 css를 설정할 수 있다.
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+  
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget : {value}
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value)
-  };
+  
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} type="text" placeholder="userName" />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { ThemeProvider } from 'styled-components';
+import {lightTheme, darkTheme} from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    {/* ThemeProvider를 통해 테마를 지정해줄 수 있도록한다. */}
+    <ThemeProvider theme={darkTheme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 

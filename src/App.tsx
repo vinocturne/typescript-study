408899cc25f483edd75970e3219236f39a6773dd
Ghunhee Shin <hago@rocketstaff.com>
2022-04-21 15:03:49 +0900

    Feat(setup) : "추가 typescript setup"
    typescript setup.
    
    새로운 프로젝트를 진행할 시
    npx create-react-app (프로젝트 명) --template typescript
    을 통해 타입스크립트 지정이 가능.
    
    기존 프로젝트 변경 시
    npm install --save typescript @types/node @types/react @types/react-dom @types/jest
    로 설치를 하고 이후 tsconfig.json파일이 없다면 추가시켜줄 수 있도록.
    
    @types(DefinitelyTyped)에는 수천개가 넘는 유명한 라이브러리들의 타입들을 정리해놓은 곳으로, ts로 만들어진 라이브러리가 아닐 경우 해당 @types/ 에 해당 라이브러리가 있는지 확인하고 설치해서 사용하면 된다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  //ThemeProvider로 설정하게 되면 해당 properties를 받아올 수 있다.
+  //props.theme.속성명으로 해당 값을 css에 적용시킬 수 있다.
+  background-color: ${(props) => props.theme.backgroundColor}
+`
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
   );
 }
 

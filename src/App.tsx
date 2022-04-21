72bf0b7c9fb0d0ddf4de05f8f63d34a9ac95803e
Ghunhee Shin <hago@rocketstaff.com>
2022-04-21 16:38:31 +0900

    Feat(form) : "추가 typescript를 활용한 form 사용"
    typescript를 사용하여 form을 사용할 때, 각 태그에서 event를 통해 받아오는 것이 있을 때에는 event의 타입값을 React.FormEvent를 활용하여 해당 태그에 맞는 이벤트 타입을 받아오도록 해야한다.
    각 이벤트 타입을 확실하게 명시하기 때문에 다른 오류가 나왔을 때 바로바로 확인이 가능하고, 태그마다 어떠한 타입으로 이벤트를 넘기는지에 대해서는 공식문서를 참조하여 확인할 수 있도록 한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget : {value}
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value)
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="white" />
-      <Circle bgColor="tomato" text="text props"/>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="userName" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
